0a3704bbe2624dee26b98cff324fc724a362280a
Michael Strother <sportflier@live.com>
2022-03-20 19:00:18 -0400

    Work on design system.

---
+++ ct-hanggliding.org/src/Pages/Home.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Home.scss';
+
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <div className="home surface-light-gradient text-normal">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa quos pariatur nobis quo, aperiam similique alias beatae aut itaque quaerat architecto dolorem doloribus ullam expedita nemo sed, obcaecati veritatis rerum?
+                Enim deleniti alias nihil autem est assumenda dicta doloremque nesciunt officiis error, incidunt at vel perferendis? Hic consequatur sed beatae animi, quasi cumque aliquam optio deleniti corporis dolorum, ratione fuga?
+                Magni illum quas praesentium odio maiores eveniet similique sit dolorum nemo provident libero neque, optio quod, deserunt mollitia nihil exercitationem soluta ipsum odit quo asperiores. Incidunt, alias? Adipisci, beatae placeat!
+                Eius assumenda, voluptatem praesentium fugit natus libero possimus. Aut repudiandae corporis asperiores minus hic ea. Ipsum temporibus rerum minus hic, quasi maxime aperiam! Aliquam cupiditate ullam aperiam dolor aut hic!
+                Officia eum libero obcaecati doloribus saepe autem quam fuga, ipsum hic. Excepturi dolorum eligendi fugit architecto consectetur cumque, praesentium tenetur, sapiente quo necessitatibus repellat, nulla nihil exercitationem neque enim doloribus!
+
+            </div>
+        </>
+    )
+};
+
+export default Home;
