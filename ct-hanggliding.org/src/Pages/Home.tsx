e14eb0d4dc91da9c206b5eb99057c3d2fa6138c8
Michael Strother <sportflier@live.com>
2022-03-21 19:27:33 -0400

    + CHGA logo

--- ct-hanggliding.org/src/Pages/Home.tsx
+++ ct-hanggliding.org/src/Pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Home.scss';
 import Footer from "../Components/Footer";
+import Logo from './../Assets/Images/CHGA-logo.svg';
 
 
 const Home: React.FC = () => {
@@ -12,13 +13,22 @@ const Home: React.FC = () => {
                 </div>
             </div>
             <div className="content-container home surface-light-gradient">
-                <div className="banner">
-                    <h2 className="heading-2 anim_scale-fade">Our Mission</h2>
-                </div>
-                <p className="text-normal">
-                    Established in 1974, the <strong>Connecticut Hang Gliding Association</strong> is an organization of men and women committed to the pursuit, promotion, and protection of hang gliding in New England.
 
-                </p>
+                <div className="chga-mission">
+                    <div>
+                        <img src={Logo} alt='Connecticut Hang Gliding Association' className="chga-logo" />
+                    </div>
+                    <div>
+                        <div className="banner">
+                            <h2 className="heading-2 anim_scale-fade">Our Mission</h2>
+                        </div>
+                        <p className="text-normal">
+                            Established in 1974, the <strong>Connecticut Hang Gliding Association</strong> is an organization of men and women committed to the pursuit, promotion, and protection of hang gliding in New England.
+
+                        </p>
+
+                    </div>
+                </div>
 
             </div>
             <Footer />
