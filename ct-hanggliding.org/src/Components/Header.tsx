5a43817e6796e10957e1cd3c7e98fd864b4b97e2
Michael Strother <sportflier@live.com>
2022-03-21 20:41:13 -0400

    Menu decoration.

--- ct-hanggliding.org/src/Components/Header.tsx
+++ ct-hanggliding.org/src/Components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Header.scss';
+import Glider from './../Assets/Images/glider.svg';
 
 export default function Header() {
     return (
         <div className='site-header'>
+            <img src={Glider} alt="" className='header-decoration' />
             <span>Talcott</span>
             <span>Membership</span>
             <span>Calendar</span>
