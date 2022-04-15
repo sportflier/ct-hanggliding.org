cbe70b42675286053f2eb42db6017faf21d180ad
Michael Strother <sportflier@live.com>
2022-04-15 19:21:10 -0400

    Social icon tweaks

--- ct-hanggliding.org/src/Components/Footer.tsx
+++ ct-hanggliding.org/src/Components/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
     return (
         <footer className="site-footer">
 
-            <a href="https://www.facebook.com/groups/364265017973">
+            <a href="https://www.facebook.com/groups/364265017973" title="Visit our Facebook page" target="_blank" rel="noreferrer">
                 <img src={IconFB} alt="FaceBook" />
             </a>
-            <a href="https://discord.gg/MVzXN237FS">
+            <a href="https://discord.gg/MVzXN237FS" title="Join our Discord server" target="_blank" rel="noreferrer">
                 <img src={IconDiscord} alt="Discord" />
             </a>
-            <a href="https://groups.io/g/HangGlideConnecticut">
+            <a href="https://groups.io/g/HangGlideConnecticut" title="Join our message group" target="_blank" rel="noreferrer">
                 <img src={IconEmail} alt="Email" />
             </a>
         </footer>
